e8bbe3ad05593b221131c3c160d441804ff8c0e5
teteuhere <matheuss.cirq@gmail.com>
2025-09-22 22:47:41 -0300

    FIX: Melhor segurança da chave secreta do Django.

--- admin/settings.py
+++ admin/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%c^m1+_+hi@=6mk^q2yr-hnqec84-$c(t*j^^f#tm-w%&am(sq'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY') 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
