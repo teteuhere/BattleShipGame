e92606b73fdad68a7fc6fff669d6118efefdb6ab
teteuhere <matheuss.cirq@gmail.com>
2025-09-22 23:12:13 -0300

    FIX: Novo script de inicialização

--- start.sh
+++ start.sh
@@ -5,6 +5,12 @@ echo "Reconstruindo e implantando os navios (containers)..."
 docker-compose up -d --build
 
 echo "Iniciando o painel de controle (frontend)..."
-cd frontend && npm run dev
+cd frontend
+
+echo "Instalando as dependências do frontend (npm install)..."
+npm install
+
+echo "Ligando o servidor do frontend (npm run dev)..."
+npm run dev
 
 echo "Todos a bordo! A aplicação está no ar em http://localhost:5173"
