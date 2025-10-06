74ebe9ec1282615553ddc8d2299a9d0ebb0190ef
Ronin <matheuss.cirq@gmail.com>
2025-10-06 01:27:07 -0300

    FIX ISSUES 6 of october 2024.

--- admin/settings.py
+++ admin/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.0.14', 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -122,5 +122,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    "http://192.168.0.14:5173",
 ]
 
